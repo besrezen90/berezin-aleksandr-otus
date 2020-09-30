ac2a9691e441347660eaa67803c59fe3cd56f2be
Aleksandr Berezin <aleksandr.berezin@mytona.com>
2020-09-30 19:08:28 +0300

    init

---
+++ node_23/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+const environment = process.env.NODE_ENV || 'development';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_WRITE_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
