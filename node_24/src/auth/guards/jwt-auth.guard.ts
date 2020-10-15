973e4bdaf61576f030ebd1ac00e892880b53c1e9
Aleksandr Berezin <aleksandr.berezin@mytona.com>
2020-10-15 21:41:22 +0300

    fix

--- node_24/src/auth/guards/jwt-auth.guard.ts
+++ node_24/src/auth/guards/jwt-auth.guard.ts
@@ -1,14 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AuthController } from '../auth.controller';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  // TODO: Реализовать эту проверку через глобал гуард
-  //   canActivate(context: ExecutionContext) {
-  //     const Controller = context.getClass();
-  //     if (new Controller() instanceof AuthController) {
-  //       return true;
-  //     }
-  //   }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
